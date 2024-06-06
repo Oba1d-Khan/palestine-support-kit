1f091529e4a3a592328a9ffdc1c4b06800a66eeb
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2024-06-06 20:20:45 +0500

    add styling props for Button styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,25 +1,32 @@
 import * as React from "react";
-import { ReactNode } from "react";
+import { ReactNode, CSSProperties } from "react";
 
 type PropsType = {
     children: React.ReactNode;
+    style: React.CSSProperties;
 };
 
-const Button = ({ children }: PropsType) => {
+const Button = ({ children, style }: PropsType) => {
     return (
         <button
             style={{
-                backgroundColor: "green",
-                color: "black",
+                backgroundColor: "#00a651",
+                color: "#ffffff",
                 padding: "10px 20px",
                 borderRadius: "5px",
                 border: "none",
                 cursor: "pointer",
                 fontSize: "16px",
                 fontWeight: "bold",
-                transition: "all 0.3s ease",
+                transition: "all 0.8s ease",
                 boxShadow: "0 2px 4px rgba(0, 0, 0, 0.3)",
+                ":hover": {
+                    backgroundColor: "#000000",
+                    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.5)",
+                },
+                ...style,
             }}
+
         >
             {children}
         </button>
