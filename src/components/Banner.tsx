aeae30ce367ddba26da31c4642b36f9e02ba7b58
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2024-06-07 12:49:20 +0500

    fix css module imports

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,22 +1,23 @@
-import * as React from 'react'
+import * as React from 'react';
+import styles from './Banner.module.css';
 
 const Banner = () => {
     return (
-        <div className="container">
-            <div className="bg"></div>
-            <div className="content">
+        <div className={styles.container}>
+            <div className={styles.bg}></div>
+            <div className={styles.content}>
                 <p>
                     <strong>Palestinian Lives Matter</strong>
-                    <svg viewBox="0 0 2 2" className="icon"><circle cx="1" cy="1" r="1" /></svg>
+                    <svg viewBox="0 0 2 2" className={styles.icon}><circle cx="1" cy="1" r="1" /></svg>
                     Showing solidarity and love with Palestinian lives.
                 </p>
-                <a href="https://www.pcrf.net/" target="_blank" rel="noopener noreferrer" className="button">
+                <a href="https://www.pcrf.net/" target="_blank" rel="noopener noreferrer" className={styles.button}>
                     Donate Now <span>&rarr;</span>
                 </a>
             </div>
-            <div className="close-button">
+            <div className={styles.closeButton}>
                 <button type="button">
-                    <svg className="close-icon" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <svg className={styles.closeIcon} viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
                     </svg>
                 </button>
@@ -25,4 +26,4 @@ const Banner = () => {
     )
 }
 
-export { Banner }
+export { Banner };
