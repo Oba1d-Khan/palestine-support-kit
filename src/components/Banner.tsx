423710248f2a963895ffa2b433a31ce981f1d884
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2024-06-09 13:51:43 +0500

    update banner ui

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import styles from './Banner.module.css';
+import * as React from "react";
+import styles from "./Banner.module.css";
 
 const Banner = () => {
     return (
@@ -7,23 +7,25 @@ const Banner = () => {
             <div className={styles.bg}></div>
             <div className={styles.content}>
                 <p>
-                    <strong>Palestinian Lives Matter</strong>
-                    <svg viewBox="0 0 2 2" className={styles.icon}><circle cx="1" cy="1" r="1" /></svg>
-                    Showing solidarity and love with Palestinian lives.
+                    <img src="./Flag_of_Palestine.png" alt="" className={styles.flag} />
+                    <strong>
+                        Palestinian Lives Matter <span>-</span>
+                    </strong>
+                    <p className={styles.text}>
+                        Showing solidarity and love with Palestinian lives!
+                    </p>
                 </p>
-                <a href="https://www.pcrf.net/" target="_blank" rel="noopener noreferrer" className={styles.button}>
+                <a
+                    href="https://www.pcrf.net/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={styles.donate}
+                >
                     Donate Now <span>&rarr;</span>
                 </a>
             </div>
-            <div className={styles.closeButton}>
-                <button type="button">
-                    <svg className={styles.closeIcon} viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
-                    </svg>
-                </button>
-            </div>
         </div>
-    )
-}
+    );
+};
 
 export { Banner };
