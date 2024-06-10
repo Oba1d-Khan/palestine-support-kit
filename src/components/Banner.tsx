966072af9c42c46c0da6116340f62b814084ece0
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2024-06-10 20:19:27 +0500

    fix styling

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -11,10 +11,10 @@ const Banner: React.FC<BannerProps> = ({ variant }) => {
 
     return (
         <div className={`container p-4 text-center relative flex items-center justify-center gap-6 overflow-hidden ${containerClasses}`}>
-            <div className={`bg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 filter blur-xl w-full h-full opacity-30 z-0 bg-gradient-to-r from-red-500 to-green-500`}></div>
-            <div className={`content flex items-center gap-3 z-10`}>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 filter blur-xl w-full h-full opacity-30 z-0 bg-gradient-to-r from-red-500 to-green-500"></div>
+            <div className="content flex items-center gap-3 z-10">
                 <p className="text-lg font-medium flex items-center">
-                    <img src="./Flag_of_Palestine.png" alt="" className="flag w-8 h-5 mx-2" />
+                    <img src="./Flag_of_Palestine.png" alt="Palestine Flag" className="w-8 h-5 mx-2" />
                     <strong className="mx-2">Palestinian Lives Matter</strong>
                     <span className="mx-2">- Showing solidarity and love with Palestinian lives!</span>
                 </p>
