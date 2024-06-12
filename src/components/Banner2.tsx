4aea059d69d50e2b7e42f4a45fda813f36994dba
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2024-06-12 23:23:54 +0500

    remove tw- prefix

--- src/components/Banner2.tsx
+++ src/components/Banner2.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 const Banner2 = () => {
     return (
-        <div className="tw-relative tw-flex tw-items-center tw-justify-center tw-px-4 tw-py-1 tw-overflow-hidden tw-text-center md:tw-py-[0.18rem] md:tw-px-0 tw-gap-14 tw-max-h-9 md:tw-max-h-10 ">
-            <div className=" tw-absolute tw-inset-0 tw-z-0 tw-w-full tw-h-full tw-bg-center  md:tw-bg-cover tw-bg-[url('/src/assets/illustration.png')] tw-blur-[1px]  tw-opacity-30"></div>
+        <div className="relative flex items-center justify-center px-4 py-1 overflow-hidden text-center md:py-[0.18rem] md:px-0 gap-14 max-h-9 md:max-h-10 ">
+            <div className="absolute inset-0 z-0 w-full h-full bg-center md:bg-cover bg-[url('/src/assets/illustration.png')] blur-[1px] opacity-30"></div>
 
-            <div className=" tw-z-10 tw-flex tw-items-center md:tw-px-2 tw-bg-white tw-rounded-full md:tw-gap-12 tw-backdrop-blur-lg">
-                <p className=" tw-flex tw-items-center tw-text-base tw-font-medium tw-leading-6 tw-tracking-wide tw-text-gray-700">
-                    <a href="" className="tw-px-4 tw-cursor-default">
+            <div className="z-10 flex items-center md:px-2 bg-white rounded-full md:gap-12 backdrop-blur-lg">
+                <p className="flex items-center text-base font-medium leading-6 tracking-wide text-gray-700">
+                    <a href="" className="px-4 cursor-default">
                         <img
                             src="https://cdn3.emoji.gg/emojis/1893-palestine-flag.png"
                             width="32px"
@@ -14,7 +14,7 @@ const Banner2 = () => {
                             alt="Palestine_Flag"
                         />
                     </a>
-                    <strong className="tw-mr-2 tw-text-xs  tw-font-bold tw-tracking-wider tw-text-black md:tw-mr-4 tw-text-start md:tw-text-sm tw-max-w-[40vw]">
+                    <strong className="mr-2 text-xs font-bold tracking-wider text-black md:mr-4 text-start md:text-sm max-w-[40vw]">
                         We stand with Palestine!
                     </strong>
                 </p>
@@ -22,13 +22,13 @@ const Banner2 = () => {
                     href="https://www.pcrf.net/"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="tw-flex tw-gap-2 tw-px-2 tw-py-1 tw-text-[0.6rem] tw-font-medium tw-text-red-600 tw-transition tw-duration-300 tw-transform tw-rounded-full md:tw-text-sm md:tw-px-3 hover:tw-scale-105 hover:tw-bg-red-100 "
+                    className="flex gap-2 px-2 py-1 text-[0.6rem] font-medium text-red-600 transition duration-300 transform rounded-full md:text-sm md:px-3 hover:scale-105 hover:bg-red-100 "
                 >
                     <div className="">
-                        <div className="tw-relative tw-inline-flex">
-                            <div className="tw-w-2 tw-h-2 tw-bg-red-500 tw-rounded-full"></div>
-                            <div className="tw-absolute tw-top-0 tw-left-0 tw-w-2 tw-h-2 tw-bg-red-500 tw-rounded-full tw-animate-ping"></div>
-                            <div className="tw-absolute tw-top-0 tw-left-0 tw-w-2 tw-h-2 tw-bg-red-500 tw-rounded-full tw-animate-pulse"></div>
+                        <div className="relative inline-flex">
+                            <div className="w-2 h-2 bg-red-500 rounded-full"></div>
+                            <div className="absolute top-0 left-0 w-2 h-2 bg-red-500 rounded-full animate-ping"></div>
+                            <div className="absolute top-0 left-0 w-2 h-2 bg-red-500 rounded-full animate-pulse"></div>
                         </div>
                     </div>
                     Donate Now
