d080de71afb519e01f5bea226e45f9a47c3f2086
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2024-06-17 23:20:28 +0500

    fix close icon layout

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -9,8 +9,7 @@ const Banner = () => {
         setVisible(false);
     };
     return (
-        <>
-            visible && (
+        visible && (
             <div className="banner-container">
                 <div className="banner-bg"></div>
                 <div className="banner-content">
@@ -38,43 +37,13 @@ const Banner = () => {
                         </div>
                         Donate Now
                     </a>
-                    <button className="close-button" onClick={handleClose}>
-                        &times;
-                    </button>
                 </div>
+                <button className="close-button" onClick={handleClose}>
+                    &times;
+                </button>
             </div>
-            )
-        </>
-        // <div className={styles.container}>
-        //     <div className={styles.bg}></div>
-        //     <div className={styles.content}>
-        //         <p>
-        //             <a href="https://emoji.gg/emoji/1893-palestine-flag" className={styles.flag}>
-        //                 <img
-        //                     src="https://cdn3.emoji.gg/emojis/1893-palestine-flag.png"
-        //                     width="32px"
-        //                     height="32px"
-        //                     alt="Palestine_Flag"
-        //                 />
-        //             </a>{" "}
-        //             <strong>
-        //                 Palestinian Lives Matter <span>-</span>
-        //             </strong>
-        //             <p className={styles.text}>
-        //                 Showing solidarity and love with Palestinian lives!
-        //             </p>
-        //         </p>
-        //         <a
-        //             href="https://www.pcrf.net/"
-        //             target="_blank"
-        //             rel="noopener noreferrer"
-        //             className={styles.donate}
-        //         >
-        //             Donate Now <span>&rarr;</span>
-        //         </a>
-        //     </div>
-        // </div >
-    );
+        )
+    )
 };
 
 export { Banner };
